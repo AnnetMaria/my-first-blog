4882169ab883ff7a3db754bdd1036442bd2eaae6
Tinkelbel <A.M.Boog@gmail.com>
2015-10-17 20:10:03 +0200

    site teruggebracht tot een pagina

--- blog/static/js/script.js
+++ blog/static/js/script.js
@@ -1,39 +1,17 @@
 $(document).ready(function(){
 
-  var start = 1;
-
-      $('#banner').css('display', 'none');
-
   var selectors = ["welcome", "ICT", 'TQC', 'philosopher'];
 
   selectors.forEach(function(item, index, array){
     $("#"+item).mouseenter(function(){
-      if (start > 0) {
-        start = start -1;
-      } else {
-        $('.banner').css('display', 'none')
-        $('#banner').css('display', 'inherit');
       selectMenuItem(item);
-    }
-
-    });
+      });
+      $("#"+item).click(function(){
+        selectMenuItem(item);
+        });
   });
 
-  $(".welcome #vlak2a").mouseenter(function(){
-    $("#contacttext").text("annet.boog@sogeti.com");
-  });
 
-  $(".klikvlak").click(function(){
-      $('#banner').css('display', 'none');
-      $('.banner').css('display', 'inherit')
-
-  });
-
-    $("#banner").mouseleave(function(){
-        $('#banner').css('display', 'none');
-        $('.banner').css('display', 'inherit')
-
-    });
 
 
   function selectMenuItem(divId){
@@ -42,24 +20,7 @@ $(document).ready(function(){
       //console.log("DivID: "+divId);
       if (item === divId){
         //console.log("ok");
-        if (item === "welcome"){
-          $("."+ item + " " + "#vlak1a").css("background-color", "#D90118");
-          $("."+ item + " " + "#vlak2a").css("background-color", "#36939B");
-          $("."+ item + " " + "#vlak2b").css("background-color", "#98D8C8");
-        } else if (item === "ICT") {
-          $("."+ item + " " + "#vlak1a").css("background-color", "#D90118");
-          $("."+ item + " " + "#vlak2a").css("background-color", "#F6E497");
-          $("."+ item + " " + "#vlak2b").css("background-color", "#FF5A2A");
-          $("#" + item).css("font-size", "1.7em");
-        }else if (item === "TQC") {
-          $("."+ item + " " + "#vlak1a").css("background-color", "#ff4019");
-          $("."+ item + " " + "#vlak2a").css("background-color", "#474030");
-          $("."+ item + " " + "#vlak2b").css("background-color", "#ff4019");
-          $("#" + item).css("font-size", "1.7em");
-        }else if (item === "philosopher") {
-          $("."+ item + " " + "#vlak1a").css("background-color", "#36939B");
-          $("."+ item + " " + "#vlak2a").css("background-color", "#B3E0C1");
-          $("."+ item + " " + "#vlak2b").css("background-color", "#98D8C8");
+        if (item != "welcome"){
           $("#" + item).css("font-size", "1.7em");
         }
         $("." + item).css("display", "inherit");
